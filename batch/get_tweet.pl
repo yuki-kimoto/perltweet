30256437b7c1f59b9333cbc3d45efbd789115173
Yuki Kimoto <kimoto.yuki@gmail.com>
2013-11-21 12:51:55 +0900

    fix get_tweet.pl not working bug

--- batch/get_tweet.pl
+++ batch/get_tweet.pl
@@ -2,11 +2,11 @@
 
 use strict;
 use warnings;
+use utf8;
 use FindBin;
 use lib "$FindBin::Bin/../extlib/lib/perl5";
 use Mojo::Server;
 use Net::Twitter::Lite::WithAPIv1_1;
-use bigint;
 
 # Config
 my $app = Mojo::Server->new->load_app("$FindBin::Bin/../script/perltweet");
@@ -16,10 +16,9 @@ my $config = $app->config;
 my $nt = Net::Twitter::Lite::WithAPIv1_1->new(
   consumer_key => $config->{twitter_consumer_key},
   consumer_secret => $config->{twitter_consumer_secret},
-  legacy_lists_api => 0
+  access_token        => $config->{twitter_access_token},
+  access_token_secret => $config->{twitter_access_token_secret}
 );
-$nt->access_token($config->{twitter_access_token});
-$nt->access_token_secret($config->{twitter_access_token_secret});
 
 # Search
 my $query = {q => 'perl', count => 1, lang => 'ja', result_type => 'recent'};
