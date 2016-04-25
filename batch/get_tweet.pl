dcef23daaab9d48a24755fb3042f01cfb9f1b901
Yuki Kimoto <kimoto.yuki@gmail.com>
2016-04-25 20:52:56 +0900

    英語の情報も見れるようにした

--- batch/get_tweet.pl
+++ batch/get_tweet.pl
@@ -30,7 +30,7 @@ my $nt = Net::Twitter::Lite::WithAPIv1_1->new(
 my $max_id = $dbi->model('tweet')->select('max(id)')->value // 0;
 
 # Langages
-my $languages = ['ja', 'en', 'zh', 'es', 'de', 'fr'];
+my $languages = ['ja', 'en'];
 
 my $url_re = qr#http://[\/\.a-zA-Z0-9]+#;
 
