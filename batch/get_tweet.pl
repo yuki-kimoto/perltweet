a9aae98f363e3aead20e3cb224ba7cfbab5bdcde
Yuki Kimoto <kimoto.yuki@gmail.com>
2018-05-31 22:43:24 +0900

    Perl様を追加

--- batch/get_tweet.pl
+++ batch/get_tweet.pl
@@ -78,6 +78,9 @@ for my $language (@$languages) {
     # Skip when perl6 contains
     next if $text =~ /perl\s*6/i;
 
+    # Skip when Perl様 contains
+    next if $text =~ /Perl様/i;
+
     # Skip when ruby contains
     next if $text =~ /ruby/i;
 
